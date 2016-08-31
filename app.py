b796f484956a9c9c6a066adcf79b267d068e2deb
Sam Heybey <sam@heybey.org>
2016-08-31 00:13:36 -0400

    fix login

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 
 class DefaultConfiguration():
 	APPLICATION_ROOT = "/"
+	SECRET_KEY = "hello"
 
 app.config.from_object(DefaultConfiguration)
 app.config.from_pyfile("app.cfg", silent=True)
+
+app.secret_key = app.config["SECRET_KEY"]
 
 @app.route("/")
 def index():
@@ -21,13 +24,16 @@ def login():
 		md5 = hashlib.md5()
 		md5.update(os.urandom(32))
 		secret = md5.hexdigest()
+		session["secret"] = secret
 		return render_template("login.html", secret=secret)
+
 	if "signature" not in request.form:
-		return redirect(url_for(login))
-	try:
-		secret = get_flashed_messages()[0]
-	except IndexError:
-		return redirect(url_for(index))
+		flash("No signature", "error")
+		return redirect(url_for("login"))
+
+	secret = session.pop("secret", None)
+	if secret is None:
+		return redirect(url_for("login"))
 
 	signature_filename = tempfile.mkstemp()[1]
 	try:
@@ -37,13 +43,13 @@ def login():
 		verified = gpg.verify_data(signature_filename, secret)
 		if not verified:
 			flash("Could not verify signature", "error")
-			return redirect(url_for(login))
+			return redirect(url_for("login"))
 		session["logged_in"] = True
-		return redirect(url_for(index))
+		return redirect(url_for("index"))
 
 	except UnicodeDecodeError:
 		flash("Invalid signature", "error")
-		return redirect(url_for(login))
+		return redirect(url_for("login"))
 	finally:
 		os.unlink(signature_filename)
 	
@@ -52,5 +58,4 @@ def login():
 @app.route("/logout")
 def logout():
 	session["logged_in"] = False
-	session.pop("secret", None)
-	return redirect(url_for(index))
+	return redirect(url_for("index"))
