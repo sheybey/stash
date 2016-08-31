9696d3cb9f5abf36353abd8d6874175e08ab91b7
Sam Heybey <sam@heybey.org>
2016-08-30 21:11:18 -0400

    add configuration

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, url_for
 app = Flask(__name__, instance_relative_config=True)
 
+class DefaultConfiguration():
+	APPLICATION_ROOT = "/"
+
+app.config.from_object(DefaultConfiguration)
+app.config.from_pyfile("app.cfg", silent=True)
+
 @app.route("/")
 def index():
 	return repr(app.config["APPLICATION_ROOT"])
