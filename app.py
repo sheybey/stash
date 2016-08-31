080fdc20bc5d1023d98884631fa9c7b5c4ab41bc
Sam Heybey <sam@heybey.org>
2016-08-30 22:28:44 -0400

    start login system

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, session, render_template
+from flask import Flask, url_for, request, session, render_template
+import hashlib
 app = Flask(__name__)
 
 class DefaultConfiguration():
@@ -9,4 +10,19 @@ app.config.from_pyfile("app.cfg", silent=True)
 
 @app.route("/")
 def index():
-	return render_template("index.html", app=app)
+	return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == "GET":
+		md5 = hashlib.md5()
+		md5.update(os.urandom(32))
+		secret = md5.hexdigest()
+		return render_template("login.html", secret=secret)
+	return "would validate here, but..."
+
+@app.route("/logout")
+def logout():
+	session["logged_in"] = False
+	session.pop("secret", None)
+	return redirect(url_for(index))
