eb01e4e271b7bf2845b438bf47181e766d4352d7
Sam Heybey <sam@heybey.org>
2016-08-30 23:21:08 -0400

    implement login

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, url_for, request, session, render_template
+from flask import Flask, url_for, request, session, render_template, redirect,
+	flash, get_flashed_messages
 import hashlib
 import os
+import gnupg
+import tempfile
 app = Flask(__name__)
 
 class DefaultConfiguration():
@@ -20,7 +23,32 @@ def login():
 		md5.update(os.urandom(32))
 		secret = md5.hexdigest()
 		return render_template("login.html", secret=secret)
-	return "would validate here, but..."
+	if "signature" not in request.form:
+		return redirect(url_for(login))
+	try:
+		secret = get_flashed_messages()[0]
+	except IndexError:
+		return redirect(url_for(index))
+
+	signature_filename = tempfile.mkstemp()[1]
+	try:
+		with open(signature_filename, "w") as signature:
+			signature.write(request.form["signature"].encode("ascii"))
+		gpg = gnupg.GPG()
+		verified = gpg.verify_data(signature_filename, secret)
+		if not verified:
+			flash("Could not verify signature", "error")
+			return redirect(url_for(login))
+		session["logged_in"] = True
+		return redirect(url_for(index))
+
+	except UnicodeDecodeError:
+		flash("Invalid signature", "error")
+		return redirect(url_for(login))
+	finally:
+		os.unlink(signature_filename)
+	
+
 
 @app.route("/logout")
 def logout():
